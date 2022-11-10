3be99b2fb021e7912b13e62857119d358b1d300e
Harsh Davda <harsh.davda@iflair.com>
2022-11-10 18:14:18 +0530

    set socket io

--- socket/index.js
+++ socket/index.js
@@ -1,10 +1,23 @@
 module.exports = (io) => {
-
+    let x = true;
     io.on('connection', socket => {
-
+        sendata(socket)
         console.log('new connection'); 
         
-		socket.on('disconnect', () => console.log('disconnected')); 
+		// socket.on('disconnect', () => console.log('disconnected')); 
 		
 	})
+
+    function sendata(socket){
+        if(x){
+            socket.emit('data1');
+        }else {
+            socket.emit('data2');
+            x = !x;
+        }
+        console.log(`the socket io ${x}`);
+        setTimeout(() => {
+            sendata(socket);
+        }, 3000);
+    }
 }
